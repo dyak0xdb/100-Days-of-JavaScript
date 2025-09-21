a28f826752ef2eea5bec64ddbd63cc06a3450dab
0xdb <141274775+dyak0xdb@users.noreply.github.com>
2025-09-21 23:58:29 +0330

    Create challenge-1.js

---
+++ day-1/challenge-1.js
@@ -0,0 +1,13 @@
+let dyak0xdb = '0xdb';       // ✅ String variable
+var age = 21;               // ✅ Number variable
+const birtyear = 2004;      // ✅ Const Number variable
+
+if(true){
+    var insideBlock = "I am inside"; // ✅ var inside if-block
+}
+console.log(insideBlock);   // ✅ var اجازه چاپ خارج از بلوک را می‌دهد
+
+let isStudent = true;       // ✅ Boolean variable
+
+let nothingHere;            
+nothingHere = "asd";        // ✅ undefined و بعد مقداردهی
