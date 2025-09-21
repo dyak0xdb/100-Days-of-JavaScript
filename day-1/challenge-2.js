d0051d21f0200f67570b331f7d34cccf4c3da79c
0xdb <141274775+dyak0xdb@users.noreply.github.com>
2025-09-21 23:59:50 +0330

    Create challenge-2.js

---
+++ day-1/challenge-2.js
@@ -0,0 +1,24 @@
+let firstName = 'dyak';
+const lastName = '0xdb';
+var age = 21;
+
+if(age > 18){
+    let canVote = true;
+    var insideVar = 'I am inside if';
+
+    console.log(canVote);
+}
+
+console.log(`${firstName} ${lastName}`); 
+console.log(insideVar); 
+
+let num = 1.23244343;
+console.log(num); 
+let isStudent = true;
+if(isStudent === true){
+    console.log('You are a student'); 
+}
+
+let nothingHere;
+if(nothingHere === undefined){
+    console.log('Variable is undefined');
